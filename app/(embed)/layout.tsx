6ee3ddb150e9ed6e1659d9b3bc391e2d63e3c32c
Chinthana-Weligamage <chinthanaweligamage1@gmail.com>
2026-05-08 11:18:31 +0530

    Add EmbedLayout component and implement ReadyToInvestPage with sector statistics

---
+++ app/(embed)/layout.tsx
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+export default function EmbedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+      <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
+      <Script src="/assets/vendor/aos/aos.js" strategy="afterInteractive" />
+      <Script src="/assets/vendor/purecounter/purecounter_vanilla.js" strategy="afterInteractive" />
+      <Script src="/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js" strategy="afterInteractive" />
+      <Script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js" strategy="afterInteractive" />
+      <Script src="/assets/vendor/swiper/swiper-bundle.min.js" strategy="afterInteractive" />
+      <Script src="/assets/vendor/glightbox/js/glightbox.min.js" strategy="afterInteractive" />
+      <Script src="/assets/js/main.js" strategy="afterInteractive" />
+    </>
+  );
+}
